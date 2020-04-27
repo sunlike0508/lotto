59ac8cad3eeda18f7990c15affdd726a4e60f275
sunlike0508 <thesin1989@gmail.com>
2020-04-27 15:45:07 +0900

    숫자 중복 검사 실패하는 테스트 코드 작성

--- src/lotto/lottoTest.java
+++ src/lotto/lottoTest.java
@@ -44,9 +44,14 @@ class lottoTest {
 	}
 	
 	@Test
-	@DisplayName("숫자 배열 중복 검사하는 메소드")
+	@DisplayName("숫자 배열 중복 검사 하는 메소드. 결과 값은 성공")
 	void testShouldReturnSuccessCheckNonoverlapNumberOfSix() {
 		assertTrue(lotto.validateNonoverlapNumberList(new int[] {1,2,3,4,5,6}));
 	}
 
+	@Test
+	@DisplayName("숫자 배열 중복 검사하는 메소드. 결과 값을 실패")
+	void testShouldReturnFailCheckNonoverlapNumberOfSix() {
+		assertFalse(lotto.validateNonoverlapNumberList(new int[] {1,2,3,4,5,1}));
+	}
 }
