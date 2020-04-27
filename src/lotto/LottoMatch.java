3512f4cd77f85a038f695573a122681472c76267
sunlike0508 <thesin1989@gmail.com>
2020-04-27 20:39:11 +0900

    리펙토링

---
+++ src/lotto/LottoMatch.java
@@ -0,0 +1,100 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.Random;
+
+class Lotto{	
+	int[] lottoNumber;
+	int bonusNumber;
+}
+
+public class LottoMatch {
+	
+	private static final int LOTTO_COUNT = 6;
+	private static final int MAX_LOTTO_NUMBER = 45;
+	
+	Lotto lotto;
+
+	public LottoMatch() {
+		lotto = createLottoNumber();
+	}
+
+	public Lotto createLottoNumber() {
+		Random random = new Random();
+		Lotto lotto = new Lotto();
+		lotto.lottoNumber = new int[LOTTO_COUNT];
+		lotto.bonusNumber = random.nextInt(MAX_LOTTO_NUMBER)+1;
+		
+		for(int i = 0; i < LOTTO_COUNT; i++) {
+			lotto.lottoNumber[i] = random.nextInt(MAX_LOTTO_NUMBER)+1;
+			
+			for(int j = 0; j < i; j++) {
+				if(lotto.lottoNumber[i] == lotto.lottoNumber[j]
+						|| lotto.lottoNumber[i] == lotto.bonusNumber) {
+					i--;
+					break;
+				}
+			}
+		}
+		
+		Arrays.sort(lotto.lottoNumber); // 정렬할 필요는 없는데 출력 시 깔끔하게 보이기 위해서 정렬
+		
+		return lotto;
+	}
+
+	public Boolean validateNonoverlapNumberList(int[] lotterNumber, int bonusNumber) {
+		
+		for(int i = 0; i < LOTTO_COUNT; i++) {
+			for(int j = i+1 ; j < LOTTO_COUNT; j++) {
+				if(lotterNumber[i] == lotterNumber[j] || lotterNumber[i] == bonusNumber) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+
+	public String matchClassLottoNumber(Lotto userlotto) {
+		return returnMatchClass(userlotto, countMatchLottoNumber(userlotto));
+	}
+	
+	private int countMatchLottoNumber(Lotto userlotto) {
+		int matchCount = 0;
+		
+		for(int i = 0; i < LOTTO_COUNT; i++) {
+			for(int j = 0; j < LOTTO_COUNT; j++) {
+				if(lotto.lottoNumber[i] == userlotto.lottoNumber[j]) {
+					matchCount++;
+				}
+			}
+		}
+		
+		return matchCount;
+	}
+
+	private String returnMatchClass(Lotto userlotto, int matchCount) {
+		if(matchCount == 6) {
+			return "1등";
+		} else if(matchCount == 5 && lotto.bonusNumber == userlotto.bonusNumber){
+			return "2등";
+		} else if(matchCount == 5){
+			return "3등";
+		} else if(matchCount == 4){
+			return "4등";
+		} else if(matchCount == 3) {
+			return "5등";
+		} else {
+			return "꽝";
+		}
+	}
+	
+	public void printLottoNumberArray(Lotto lotto) {
+		for(int number : lotto.lottoNumber) {
+			System.out.print(number + " ");
+		}
+		
+		System.out.print(lotto.bonusNumber);
+		System.out.println();
+	}
+}
--- src/lotto/LottoMatchTest.java
+++ src/lotto/LottoMatchTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class lottoTest {
+class LottoMatchTest {
 	/*
 	 	1. 로또 숫자 구성
 	 	  *숫자 범위 : 1 ~ 42
@@ -23,55 +23,55 @@ class lottoTest {
 	 	  * N개의 로또 의 당첨여부를 통계내어 출력
 	 */
 	
-	Lotto lotto;
-	LottoNumber userlottoNumber;
+	LottoMatch testlottoMatch;
+	Lotto userlotto;
 	
 	@BeforeEach
 	void testBeoreEach() {
-		lotto = new Lotto();
-		userlottoNumber = lotto.createLottoNumber();
+		testlottoMatch = new LottoMatch();
+		userlotto = testlottoMatch.createLottoNumber();
 	}
 
 	@Test
 	@DisplayName("Test create lotto object")
 	void testShouldReturnSuccessWhenLottoClassIsCreated() {
-		assertTrue(lotto != null);
+		assertTrue(testlottoMatch != null);
 	}
 	
 	@Test
 	@DisplayName("숫자 배열 중복 검사 하는 메소드. 결과 값은 성공")
 	void testShouldReturnSuccessCheckNonoverlapNumberOfSix() {
-		assertTrue(lotto.validateNonoverlapNumberList(new int[] {1,2,3,4,5,6}, 7));
+		assertTrue(testlottoMatch.validateNonoverlapNumberList(new int[] {1,2,3,4,5,6}, 7));
 	}
 
 	@Test
 	@DisplayName("숫자 배열 중복 검사하는 메소드. 결과 값을 실패")
 	void testShouldReturnFailCheckNonoverlapNumberOfSix() {
-		assertFalse(lotto.validateNonoverlapNumberList(new int[] {1,2,3,4,5,1}, 10));
+		assertFalse(testlottoMatch.validateNonoverlapNumberList(new int[] {1,2,3,4,5,1}, 10));
 	}
 	
 	@Test
 	@DisplayName("숫자 배열 중복 검사하는 메소드. 보너스 번호 중복으로 결과 값을 실패")
 	void testShouldReturnFailCheckNonoverlapNumberOfSixAndBonusNumber() {
-		assertFalse(lotto.validateNonoverlapNumberList(new int[] {1,2,3,4,5,6}, 1));
+		assertFalse(testlottoMatch.validateNonoverlapNumberList(new int[] {1,2,3,4,5,6}, 1));
 	}
 	
 	@Test
 	@DisplayName("랜덤으로 생성되는 로또 번호 중복 테스트")
 	void testShouldReturnSuccessNonoverlapRandomNumberFromUser() {
-		assertTrue(lotto.validateNonoverlapNumberList(userlottoNumber.lottoNumber, userlottoNumber.bonusNumber));
+		assertTrue(testlottoMatch.validateNonoverlapNumberList(userlotto.lottoNumber, userlotto.bonusNumber));
 	}
 	
 	@Test
 	@DisplayName("로또 번호와 자동(여기선 임의의 값) 생성된 번호를 비교하여 당첨결과 알려주는 메소드 테스트. 결과는 2등")
 	void testShouldReturnSecondClassCompareLottoNumber() {
-		Lotto lottoNumber = new Lotto();
-		lottoNumber.lottoNubmer.lottoNumber = new int[] {1,2,3,4,5,7};
-		lottoNumber.lottoNubmer.bonusNumber = 10;
+		LottoMatch lottoMatch = new LottoMatch();
+		lottoMatch.lotto.lottoNumber = new int[] {1,2,3,4,5,7};
+		lottoMatch.lotto.bonusNumber = 10;
 		
-		LottoNumber tempLottoNumber = new LottoNumber();
-		tempLottoNumber.lottoNumber = new int[] {1,2,3,4,5,6};
-		tempLottoNumber.bonusNumber = 10;
-		assertEquals(new String("2등"), lottoNumber.matchClassLottoNumber(tempLottoNumber));
+		Lotto tempLotto = new Lotto();
+		tempLotto.lottoNumber = new int[] {1,2,3,4,5,6};
+		tempLotto.bonusNumber = 10;
+		assertEquals(new String("2등"), lottoMatch.matchClassLottoNumber(tempLotto));
 	}
 }
